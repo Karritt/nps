2c7a5f6850b10714566c37d015c17f17dd12687b
Karritt <45860943+Karritt@users.noreply.github.com>
2025-02-22 13:18:43 -0700

    add menu changing

--- src/js/main.js
+++ src/js/main.js
@@ -17,11 +17,29 @@ function setMainInfo(infos){
     document.querySelector(".info").innerHTML = infoBody;
 }
 
+function enableNavigation() {
+    const button = document.querySelector("#global-nav-toggle");
+    button.addEventListener("click", e => {
+        let target = e.target;
+        if (target.tagName !== "BUTTON") {
+            target = target.closest("button");
+        }
+        if (target.getAttribute("aria-expanded") == "false") {
+            target.setAttribute("aria-expanded", "true");
+        } else {
+            target.setAttribute("aria-expanded", "false");
+        }
+        //target.querySelector("#global-nav-toggle__close").classList.toggle("hidden");
+        //target.querySelector("#global-nav-toggle__open").classList.toggle("hidden");
+    });
+}
+
 parkData.then(data => {
     console.log(data);
     setMainIntro(data);
     setHeaderFooter(data);
     console.log(getParkInfo(data));
     setMainInfo(getParkInfo(data));
+    enableNavigation();
 })
 
